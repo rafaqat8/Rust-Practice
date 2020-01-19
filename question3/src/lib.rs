2727009295e33744c4909ffb1cba601f4a965e41
rafaqat8 <59524054+rafaqat8@users.noreply.github.com>
2020-01-19 23:24:34 +0500

    Add files via upload

---
+++ question3/src/lib.rs
@@ -0,0 +1,7 @@
+pub mod weather{
+    pub mod winter{
+        pub fn temperature(){
+            println!("Its too cold in Winter and temperature is unbearable");
+        }
+    }
+}
---
+++ question3_binary/src/main.rs
@@ -0,0 +1,5 @@
+use question3;
+fn main() {
+    question3::weather::winter::temperature();
+    println!("Hello, world!");
+}
