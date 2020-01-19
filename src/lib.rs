97d594ead38d201d4546f4eccfaa24db977120e5
Syed Rafaqat Hussain <engr.rafaqathussain@gmail.com>
2020-01-19 23:16:29 +0500

    this is question 2

---
+++ src/lib.rs
@@ -0,0 +1,7 @@
+pub mod weather{
+    pub mod summer{
+        pub fn temperature(){
+            println!("The temperature of the summer is unbearable");
+        }
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,5 @@
+mod lib;
+fn main() {
+    lib::weather::summer::temperature();
+    println!("Hello, world!");
+}
